94d190e8fce04281a5906edf0abcc03706149f77
kirillbeldyaga <kirill.beldyaga@gmail.com>
2016-01-31 11:43:55 +0200

    Huffman Code Coder and Decoder

---
+++ Huffman/TestProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HuffmanLibrary;
+using System.Runtime.Serialization;
+using System.Xml;
+
+namespace TestProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Huffman huf = new Huffman("acccbbdde");
+            Console.WriteLine(huf.Encode("abc"));
+            Console.WriteLine(huf.Decode(huf.Encode("abc")));
+            Console.ReadKey();
+        }
+    }
+}
